3635bb1284a3c116990ae389114abbf9a225c2c4
MD. Hazzaz Bin Faiz <hazzazbinfaiz@gmail.com>
2022-09-12 09:40:06 +0600

    Working on vitepress

--- notes/.vitepress/config.js
+++ notes/.vitepress/config.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
@@ -6,11 +7,63 @@ export default defineConfig({
     description: 'SEU CSE Notes static site powered by Vite & Vue.',
     markdown: {
         config: md => {
-            md.use(require("markdown-it-katex"));
+            md.use(require("markdown-it-katex"))
         }
     },
     head: [
         ['link', { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css' }],
         ['link', { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/github-markdown-css/2.2.1/github-markdown.css' }]
     ],
+    themeConfig: {
+        editLink: {
+            pattern: 'https://github.com/HazzazBinFaiz/seu-cse-notes/edit/main/notes/:path',
+            text: 'Edit this page on GitHub'
+        },
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/HazzazBinFaiz/seu-cse-notes' }
+        ],
+        footer: {
+            message: 'Made with ❤️ by students of SEU',
+            copyright: 'Copyright © 2022 faculties and students of SEU'
+        },
+        sidebar: sidebar(),
+    }
 })
+
+
+function dailyNoteScanner(path, mapper = null) {
+    return fs.readdirSync('notes/'+path)
+        .filter(file =>
+            file.endsWith('.md')
+            && !['index.md', 'README.md'].includes(file)
+            && file.match(/\d{4}_\d{2}_\d{2}/)
+        ).map(file => {
+            return {
+                text: file.match(/\d{4}_\d{2}_\d{2}/)[0].replaceAll('_', '-'),
+                link: path+'/'+file.replace('.md', '')
+            }
+        }).map(mapper ? mapper : item => item)
+}
+
+function sidebar() {
+    return {
+        '/ECO461.5/': [
+            {
+                text: 'ECO461.5',
+                items: [
+                    { text: 'Table Of Content', link: '/ECO461.5/' },
+                ]
+            },
+            {
+                text: 'Exam questions',
+                items: [
+                    { text: 'Mid term', link : '/ECO461.5/_exam_mid.md'}
+                ]
+            },
+            {
+                text: 'Daily Notes',
+                items: dailyNoteScanner('ECO461.5')
+            }
+        ]
+    }
+}
--- notes/index.md
+++ notes/index.md
@@ -1,6 +1,9 @@
 ---
 layout: home
 
+title: SEU CSE Notes
+titleTemplate: SEU CSE Notes static site powered by Vite & Vue.
+
 hero:
   name: SEU CSE Notes
   text: SEU CSE Notes static site powered by Vite & Vue.
@@ -8,5 +11,13 @@ hero:
   actions:
     - theme: brand
       text: ECO461.5
-      link: /ECO461.5/README.md
+      link: /ECO461.5/
+    - theme: alt
+      text: View on GitHub
+      link: https://github.com/HazzazBinFaiz/seu-cse-notes
+
+features:
+  - icon: 💰
+    title: ECO461.5
+    details: Introduction to Economics
 ---
